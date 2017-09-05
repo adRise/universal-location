8cae4970c8c06c98cb49f55b1556007a0916eb33
André Antunes Vieira <andreantunesv@gmail.com>
2017-09-05 13:27:23 -0300

    universal-location v1.0.5 accept url mock

--- index.js
+++ index.js
@@ -14,29 +14,7 @@ function setLocationFromHeaders(req){
   }
   const headers = req.headers
   try{
-    const href = headers.referer;
-    const hrefArr = href.split('//');
-    const protocol = hrefArr[0];
-    const url = hrefArr[1]
-    const urlArr = url.split('/');
-    const host = urlArr.shift();
-    const uri = '/' + urlArr.join('/');
-    const uriArr = uri.split('?');
-    const pathname = uriArr[0];
-    const search = uriArr[1];
-    const hostArr = host.split(':');
-    const hostname = hostArr[0];
-    const port = hostArr[1];
-    exports.default = global.Location = {
-      href: href,
-      protocol: protocol,
-      host: host,
-      hostname: hostname,
-      port: port || '',
-      pathname: pathname,
-      search: search ? '?' + search : '',
-      origin: protocol + '//' + host
-    };
+    setUrl(headers.referer);
   }
   catch(e){
   }
@@ -49,11 +27,38 @@ function mustSetLocation(req){
   return !!req.headers.referer;
 }
 
+function setUrl(href){
+  const hrefArr = href.split('//');
+  const protocol = hrefArr[0];
+  const url = hrefArr[1]
+  const urlArr = url.split('/');
+  const host = urlArr.shift();
+  const uri = '/' + urlArr.join('/');
+  const uriArr = uri.split('?');
+  const pathname = uriArr[0];
+  const search = uriArr[1];
+  const hostArr = host.split(':');
+  const hostname = hostArr[0];
+  const port = hostArr[1];
+  exports.default = global.Location = {
+    href: href,
+    protocol: protocol,
+    host: host,
+    hostname: hostname,
+    port: port || '',
+    pathname: pathname,
+    search: search ? '?' + search : '',
+    origin: protocol + '//' + host
+  };
+}
+
 exports.middleware = function(){
   return function(req, res, next){
     setLocationFromHeaders(req)
     next();
   }
 };
+
+exports.setUrl = setUrl;
 
 exports.default = {}
